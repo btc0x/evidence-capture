7816677e2cbcd599756df596b39ee56f5b30c17e
huy.nv <huy.nv@japanticket.com>
2023-06-24 21:51:16 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
+// vue.config.js
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/evidence-capture/' // Thay tên repository của các bạn vào đây nhé
-      : '/'
-  }
+    publicPath: '/evidence-capture/'
+}
